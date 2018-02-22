db5f242f3107be03d4e1bed0c99884604f014bf6
sush0408 <sushil@cvtrobotics.in>
2018-02-23 04:32:17 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
-// server.js
+// Get dependencies
 const express = require('express');
-const app = express();
 const path = require('path');
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// ...
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+const http = require('http');
+//const bodyParser = require('body-parser');
+console.log("i am here1");
+// Get our API routes
+const api = require('./server/routes/api');
+
+const app = express();
+
+// Parsers for POST data
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+console.log("i am here2");
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Set our api routes
+//app.use('/api', api);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+
+/**
+ * Get port from environment and store in Express.
+ */
+//const port = process.env.PORT || '5000';
+//app.set('port', port);
+app.set('port', (process.env.PORT || 5000))
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(app.get('port'), () => console.log(`API running`));
